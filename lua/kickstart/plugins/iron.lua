51440c1371218eccee23b31403c4bf598d7c1ebe
Florian Teich <florianteich@gmail.com>
2025-11-24 20:45:57 +0100

    update iron

--- lua/kickstart/plugins/iron.lua
+++ lua/kickstart/plugins/iron.lua
@@ -10,6 +10,8 @@ return {
         scratch_repl = true,
         -- How the repl window will be displayed
         repl_open_cmd = view.bottom(40),
+        -- Send selections to the DAP repl if an nvim-dap session is running.
+        dap_integration = true,
         -- Choose your preferred REPL for each language:
         repl_definition = {
           sh = {
@@ -18,23 +20,35 @@ return {
           python = {
             command = {"python3"},
             format = common.bracketed_paste_python,
+            block_dividers = { "# %%", "#%%" },
+            env = {PYTHON_BASIC_REPL = "1"} 
           },
         },
       },
 
       keymaps = {
-        send_motion = "<leader>rs",
-        visual_send = "<leader>rs",
-        send_file = "<leader>rf",
-        send_line = "<leader>rl",
-        send_mark = "<leader>rm",
-        mark_motion = "<leader>rm",
-        mark_visual = "<leader>rm",
-        remove_mark = "<leader>rd",
-        cr = "<leader>r<cr>",
-        interrupt = "<leader>r<space>",
-        exit = "<leader>rq",
-        clear = "<leader>rc",
+        toggle_repl = "<space>rr", -- toggles the repl open and closed.
+        -- If repl_open_command is a table as above, then the following keymaps are
+        -- available
+        -- toggle_repl_with_cmd_1 = "<space>rv",
+        -- toggle_repl_with_cmd_2 = "<space>rh",
+        restart_repl = "<space>rR", -- calls `IronRestart` to restart the repl
+        send_motion = "<space>sc",
+        visual_send = "<space>sc",
+        send_file = "<space>sf",
+        send_line = "<space>sl",
+        send_paragraph = "<space>sp",
+        send_until_cursor = "<space>su",
+        send_mark = "<space>sm",
+        send_code_block = "<space>sb",
+        send_code_block_and_move = "<space>sn",
+        mark_motion = "<space>mc",
+        mark_visual = "<space>mc",
+        remove_mark = "<space>md",
+        cr = "<space>s<cr>",
+        interrupt = "<space>s<space>",
+        exit = "<space>sq",
+        clear = "<space>cl",
       },
 
       highlight = { italic = true },
